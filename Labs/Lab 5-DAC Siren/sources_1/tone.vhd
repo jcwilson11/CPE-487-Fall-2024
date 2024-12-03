076146ee59585958f0171d0bd6a2afc8f38c18ee
Joris <jcwilson2021@gmail.com>
2024-12-02 19:26:18 -0500

    added siren lab changes

--- Labs/Lab 5-DAC Siren/sources_1/tone.vhd
+++ Labs/Lab 5-DAC Siren/sources_1/tone.vhd
@@ -4,34 +4,55 @@ USE IEEE.NUMERIC_STD.ALL;
 
 -- Generates a 16-bit signed triangle wave sequence at a sampling rate determined
 -- by input clk and with a frequency of (clk*pitch)/65,536
-ENTITY tone IS
-	PORT (
-		clk : IN STD_LOGIC; -- 48.8 kHz audio sampling clock
-		pitch : IN UNSIGNED (13 DOWNTO 0); -- frequency (in units of 0.745 Hz)
-	data : OUT SIGNED (15 DOWNTO 0)); -- signed triangle wave out
+ENTITY tone is
+        PORT (    
+                clk : IN STD_LOGIC;    -- 48.8 kHz audio sampling clock
+                pitch : IN UNSIGNED (13 downto 0);  -- frequency (in units of 0.745 Hz)
+                btn_press : IN STD_LOGIC;
+                data : OUT SIGNED (15 downto 0));  -- signed triangle wave out
 END tone;
 
-ARCHITECTURE Behavioral OF tone IS
-	SIGNAL count : unsigned (15 DOWNTO 0); -- represents current phase of waveform
-	SIGNAL quad : std_logic_vector (1 DOWNTO 0); -- current quadrant of phase
-	SIGNAL index : signed (15 DOWNTO 0); -- index into current quadrant
+ARCHITECTURE Behavioral of tone is
+        SIGNAL count : unsigned (15 DOWNTO 0); -- represents current phase of waveform
+	    SIGNAL quad : std_logic_vector (1 DOWNTO 0); -- current quadrant of phase
+	    SIGNAL index : signed (15 DOWNTO 0); -- index into current quadrant
+
+        SIGNAL data_sq:SIGNED(15 downto 0);
+        SIGNAL data_tri:SIGNED(15 downto 0);
+
 BEGIN
-	-- This process adds "pitch" to the current phase every sampling period. Generates
-	-- an unsigned 16-bit sawtooth waveform. Frequency is determined by pitch. For
-	-- example when pitch=1, then frequency will be 0.745 Hz. When pitch=16,384, frequency
-	-- will be 12.2 kHz.
-	cnt_pr : PROCESS
-	BEGIN
-		WAIT UNTIL rising_edge(clk);
-		count <= count + pitch;
-	END PROCESS;
-	quad <= std_logic_vector (count (15 DOWNTO 14)); -- splits count range into 4 phases
-	index <= signed ("00" & count (13 DOWNTO 0)); -- 14-bit index into the current phase
-	-- This select statement converts an unsigned 16-bit sawtooth that ranges from 65,535
-	-- into a signed 12-bit triangle wave that ranges from -16,383 to +16,383
-	WITH quad SELECT
-	data <= index WHEN "00", -- 1st quadrant
-	        16383 - index WHEN "01", -- 2nd quadrant
-	        0 - index WHEN "10", -- 3rd quadrant
-	        index - 16383 WHEN OTHERS; -- 4th quadrant
-END Behavioral;
+        -- This process adds "pitch" to the current phase every sampling period. Generates
+        -- an unsigned 16-bit sawtooth waveform. Frequency is determined by pitch. For
+        -- example when pitch=1, then frequency will be 0.745 Hz. When pitch=16,384, frequency
+        -- will be 12.2 kHz.
+        cnt_pr: PROCESS
+        BEGIN
+            WAIT UNTIL rising_edge(clk);
+            count <= count + pitch;
+        END PROCESS;
+        quad <= std_logic_vector (count (15 downto 14)); -- splits count range into 4 phases
+        index <= signed ("00" & count (13 downto 0));  -- 14-bit index into the current phase
+        -- This select statement converts an unsigned 16-bit sawtooth that ranges from 65,535
+        -- into a signed 12-bit triangle wave that ranges from -16,383 to +16,383
+        WITH quad SELECT
+        data_sq <= to_signed(16383,16) when "00",
+        to_signed(-16383,16) when "01",
+        to_signed(16383,16) when "10",
+        to_signed(-16383,16) when others;
+        
+        WITH quad SELECT
+        data_tri <= index when "00",
+        16383 - index when "01",
+        0 - index when "10",
+        index - 16383 when others;
+
+tone_select: process
+begin --switch to sqaure wave when button is pressed, else play triangle
+ if btn_press = '1' then
+  data <= data_sq;
+ else
+  data <= data_tri;
+ end if;
+end process;
+ 
+end Behavioral;
--- Labs/Lab 5-DAC Siren/sources_1/wail.vhd
+++ Labs/Lab 5-DAC Siren/sources_1/wail.vhd
@@ -1,29 +1,30 @@
-LIBRARY IEEE;
+library IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
--- Generates a "wailing siren" sound by instancing a "tone" module and modulating
--- the pitch of the tone. The pitch is increased until it reaches hi_pitch and then
--- decreased until it reaches lo_pitch and then increased again, etc.
-ENTITY wail IS
-	PORT (
-		lo_pitch : IN UNSIGNED (13 DOWNTO 0); -- lowest pitch (in units of 0.745 Hz)
-		hi_pitch : IN UNSIGNED (13 DOWNTO 0); -- highest pitch (in units of 0.745 Hz)
-		wspeed : IN UNSIGNED (7 DOWNTO 0); -- speed of wail in pitch units/wclk
-		wclk : IN STD_LOGIC; -- wailing clock (47.6 Hz)
-		audio_clk : IN STD_LOGIC; -- audio sampling clock (48.8 kHz)
-	audio_data : OUT SIGNED (15 DOWNTO 0)); -- output audio sequence (wailing tone)
+  -- Generates a "wailing siren" sound by instancing a "tone" module and modulating
+ -- the pitch of the tone. The pitch is increased until it reaches hi_pitch and then
+ -- decreased until it reaches lo_pitch and then increased again...etc.
+ENTITY wail is
+    PORT ( 
+            lo_pitch : IN  UNSIGNED (13 downto 0); -- lowest pitch (in units of 0.745 Hz)
+            hi_pitch : IN  UNSIGNED (13 downto 0); -- highest pitch (in units of 0.745 Hz)
+            wspeed : IN UNSIGNED (7 downto 0);  -- speed of wail in pitch units/wclk
+            button_press : IN STD_LOGIC;
+            wclk : IN  STD_LOGIC;    -- wailing clock (47.6 Hz)
+            audio_clk : IN STD_LOGIC;   -- audio sampling clock (48.8 kHz)
+            audio_data : OUT  SIGNED (15 downto 0)); -- output audio sequence (wailing tone)
 END wail;
 
-ARCHITECTURE Behavioral OF wail IS
-	COMPONENT tone IS
-		PORT (
-			clk : IN STD_LOGIC;
-			pitch : IN UNSIGNED (13 DOWNTO 0);
-			data : OUT SIGNED (15 DOWNTO 0)
-		);
-	END COMPONENT;
-	SIGNAL curr_pitch : UNSIGNED (13 DOWNTO 0); -- current wailing pitch
+ARCHITECTURE Behavioral OF wail is
+        COMPONENT tone is
+                PORT (  
+                        clk : IN STD_LOGIC;      
+                        pitch : IN UNSIGNED (13 downto 0); 
+                        btn_press : IN STD_LOGIC;
+                        data : OUT SIGNED (15 downto 0)); 
+        END COMPONENT;
+        SIGNAL curr_pitch : UNSIGNED (13 DOWNTO 0); -- current wailing pitch
 BEGIN
 	-- this process modulates the current pitch. It keep a variable updn to indicate
 	-- whether tone is currently rising or falling. Each wclk period it increments
